ea3c7a3e389b23177a0be6180baaf054fbf0d3a2
Vegeta-7 <128510436+Vegeta-7@users.noreply.github.com>
2024-04-16 13:53:39 +0530

    Update diabetes_prediction.py

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -8,6 +8,7 @@ Original file is located at
 
 # Description:
 
+
 The objective of the dataset is to predict whether or not a patient has diabetes, based on certain diagnostic measurements included in the dataset.<br> <br>
 The datasets consists of several medical predictor variables and one target variable, Outcome. Predictor variables includes the number of pregnancies the patient has had, their BMI, insulin level, age, and so on.
 
